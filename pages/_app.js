61329ded9bf859316c438a31cd5ccbf8a0c3296b
a-dardanelli <agustindardanelli@toptive.co>
2022-11-27 23:09:27 -0300

    Cambios en app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import { LanguageProvider } from '../context/LanguageContext';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <LanguageProvider>
+      <Component {...pageProps} />
+    </LanguageProvider>
+  )
 }
 
 export default MyApp
