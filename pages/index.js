0f4a1615a6baf2f205584773318dd6d56fe51190
a-dardanelli <agustindardanelli@toptive.co>
2022-11-29 00:02:27 -0300

    Cambios en disñeño Resposive y DARKMODE

--- pages/index.js
+++ pages/index.js
@@ -22,11 +22,9 @@ export default function home ()  {
       </Head>
       
       <section className='bg-gray-200' >
-          <div className=' contrast-125  saturate-150' >
-            <Image 
-            href = '../public/back.png'
+          <div className=' contrast-125  saturate-150 custom-img' >
+                    
             
-            />
           <div className='block sm:p-8 lg:p-24 text-white text-center bg-stone-900 bg-opacity-50 ' >
             <p className=' no-underline border-b-teal-500 text-white text-5xl dark:text-white' >{textsLanguage.p1}</p>    
             <br/>      
